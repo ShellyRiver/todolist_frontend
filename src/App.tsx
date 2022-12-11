b5340970f1d23336dbe2d348e74d043daea3cc85
Zilinghan <Zilinghan.18@intl.zju.edu.cn>
2022-12-11 13:33:34 -0600

    change back to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   return (
     <AuthContext.Provider value={{email: email, setEmail: setEmail}}>
       <div className="App">
-        <HashRouter basename='/grouptodo'>
+        <BrowserRouter basename='/grouptodo'>
           <Routes>
             <Route path='/' element={<Navigator />}>
               <Route path='/' element={<Home />}>
@@ -44,7 +44,7 @@ function App() {
             </Route>
             {/*<Route path='/profile' element={<Profile />}/>*/}
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </div>
     </AuthContext.Provider>
   );
