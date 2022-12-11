77aa9ae27dce7410c60462c681998bc60ee3cd07
Zilinghan <Zilinghan.18@intl.zju.edu.cn>
2022-12-11 15:56:25 -0600

    rmv file

--- src/authentication/secret.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC-YXP4gMQuMxBuo5uCAu3wk5kLp2hT6lI",
-    authDomain: "grouptodo-9f335.firebaseapp.com",
-    projectId: "grouptodo-9f335",
-    storageBucket: "grouptodo-9f335.appspot.com",
-    messagingSenderId: "329704920298",
-    appId: "1:329704920298:web:679ab00e0e836c04a17393"
-};
-
